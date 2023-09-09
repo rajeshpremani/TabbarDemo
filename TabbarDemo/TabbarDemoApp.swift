da0456f96af520cf5fd63efe26eceeee481cd202
Rajesh Kumar <rajesh.kumar@veripark.com>
2023-09-09 19:25:32 +0500

    Initial Commit

---
+++ TabbarDemo/TabbarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarDemoApp.swift
+//  TabbarDemo
+//
+//  Created by Veripark on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabbarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
